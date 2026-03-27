59eb64c66e1345f764e604b8691f2a6cfbb58c1d
Neal Wang <nealwang.sh@protonmail.com>
2026-03-26 21:43:23 -0700

    feat: init scripts

---
+++ scripts/extract.js
@@ -0,0 +1,20 @@
+// Run the following file in browser devtools console
+
+const targets = [];
+
+const nodes = document.querySelectorAll("a.tabliscel-flex");
+for (const node of nodes) {
+    const rawText = node.innerText.trim();
+
+    if (rawText.startsWith("Section")) {
+        const cleanTitle = rawText.split("\n")[0].trim();
+
+        targets.push({
+            title: cleanTitle,
+            url: node.href,
+            id: node.href.split("/").pop(),
+        });
+    }
+}
+
+console.log(JSON.stringify(targets, null, 4));
